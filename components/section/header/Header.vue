d626cdb5e293b9aab64dd37c9b5f5e96a50fbd07
AmirHossein <man.of.cod3@gmail.com>
2023-11-30 00:44:04 +0330

    Header navigation drawer designed.

--- components/section/header/Header.vue
+++ components/section/header/Header.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const router = useRouter();
 const showHeader = ref(true);
-
+const drawer = ref(null);
 
 const isLoginPage = () => router.currentRoute.value.name === 'auth-login';
 
@@ -14,15 +14,20 @@ watchEffect(() => {
   <v-app-bar class="bg-light text-white " elevation="0" v-if="showHeader">
     <v-container>
       <v-row justify="space-between" align="center">
-        <div>
+        <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = <any>!drawer"/>
+        <div class="float-md-right">
           <v-img
               width="135"
               height="135"
               src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg">
           </v-img>
         </div>
-        <div >
+        <div class="hidden-sm-and-down">
           <v-row class="align-center">
+            <div class="mr-15">
+              <a class="text-white pr-5">Video courses</a>
+              <a class="text-white px-4">Meditations</a>
+            </div>
             <v-avatar image="https://cdn.vuetifyjs.com/images/john.jpg" size="45"></v-avatar>
             <p class="font-16 font-weight-light mx-4">Jalallinux</p>
             <v-btn icon="mdi mdi-logout"/>
@@ -31,6 +36,19 @@ watchEffect(() => {
       </v-row>
     </v-container>
   </v-app-bar>
+  <v-navigation-drawer
+      class="bg-light"
+      v-model="drawer"
+      temporary="">
+    <v-list-item prepend-avatar="https://cdn.vuetifyjs.com/images/john.jpg" title="John Leider"/>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav="">
+      <v-list-item prepend-icon="mdi-video-box" title="Video courses" ></v-list-item>
+      <v-list-item prepend-icon="mdi-meditation" title="Meditations" ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <style scoped>
