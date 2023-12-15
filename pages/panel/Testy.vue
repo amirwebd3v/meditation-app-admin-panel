680e889a23fce08ea5022c611c9ace948301134b
AmirHossein <man.of.cod3@gmail.com>
2023-12-15 14:48:14 +0330

    data Table refactor

--- pages/panel/Testy.vue
+++ pages/panel/Testy.vue
@@ -2,12 +2,12 @@
   <v-container fluid>
     <v-card>
       <v-text-field @keyup.enter="load({page: 1, itemsPerPage: 15})" v-model="searchText"
-                    hide-details placeholder="Search name..." class="ma-2" density="compact" />
+                    hide-details placeholder="Search name..." class="ma-2" density="compact"/>
       <v-data-table-server
           v-if="!!items"
-          :page="meta.page"
-          :items-per-page="meta.per_page"
-          :items-length="meta.total"
+          :page="currentPage"
+          :items-per-page="perPage"
+          :items-length="total"
           show-current-page
           :loading="loading"
           :headers="headers"
@@ -17,12 +17,12 @@
 
         <template #item.thumbnail="{ item }">
           <v-card class="my-2" elevation="2" rounded>
-            <v-img :src="item.thumbnail.urls.small" height="64" cover="" />
+                        <v-img :src="item.thumbnail.urls.small" height="64" cover="" />
           </v-card>
         </template>
 
         <template #item.set="{ item }">
-          {{ item.set.replace('MULTIPLE', 'COURSE') }}
+                    {{ item.set.replace('MULTIPLE', 'COURSE') }}
         </template>
 
       </v-data-table-server>
@@ -31,46 +31,90 @@
 </template>
 
 
-<script>
-import { useMeditationStore } from "~/stores/meditation.js";
-import { mapState } from "pinia";
+<!--<script>-->
+<!--import { useMeditationStore } from "~/stores/meditation.js";-->
+<!--import { mapState } from "pinia";-->
 
-export default {
-  name: "Testy",
-  setup() {
+<!--export default {-->
+<!--  name: "Testy",-->
+<!--  setup() {-->
 
-    const loading = ref(true);
-    const searchText = ref('');
-    const headers = [
-      {key: 'title', title: 'Title', align: 'start', sortable: true},
-      {key: 'set', title: 'Type', sortable: false},
-      {key: 'category', title: 'Category', sortable: false},
-      {key: 'description', title: 'Description', sortable: true},
-      {key: 'lessons_count', title: 'Quantity', sortable: true, align: 'center'},
-      {key: 'thumbnail', title: 'Picture', sortable: false, align: 'center'},
-      {key: 'price', title: 'Price', sortable: true},
-      {key: 'actions', title: '', sortable: false},
-    ];
+<!--    const loading = ref(true);-->
+<!--    const searchText = ref('');-->
+<!--    const headers = [-->
+<!--      {key: 'title', title: 'Title', align: 'start', sortable: true},-->
+<!--      {key: 'set', title: 'Type', sortable: false},-->
+<!--      {key: 'category', title: 'Category', sortable: false},-->
+<!--      {key: 'description', title: 'Description', sortable: true},-->
+<!--      {key: 'lessons_count', title: 'Quantity', sortable: true, align: 'center'},-->
+<!--      {key: 'thumbnail', title: 'Picture', sortable: false, align: 'center'},-->
+<!--      {key: 'price', title: 'Price', sortable: true},-->
+<!--      {key: 'actions', title: '', sortable: false},-->
+<!--    ];-->
 
-    const load = (options) => {
-      loading.value = true;
+<!--    const load = (options) => {-->
+<!--      loading.value = true;-->
 
-      const conditions = [
-        {key: 'title', operator: 'like', value: searchText.value}
-      ]
+<!--      const conditions = [-->
+<!--        {key: 'title', operator: 'like', value: searchText.value}-->
+<!--      ]-->
 
-      useMeditationStore().fetch(options.page, options.itemsPerPage, !!options.sortBy?.length ? options.sortBy[0].key : null, !!options.sortBy?.length ? options.sortBy[0].order : null, conditions)
-          .finally(() => {
-            loading.value = false;
-          });
-    };
+<!--      useMeditationStore().fetch(options.page, options.itemsPerPage, !!options.sortBy?.length ? options.sortBy[0].key : null, !!options.sortBy?.length ? options.sortBy[0].order : null, conditions)-->
+<!--          .finally(() => {-->
+<!--            loading.value = false;-->
+<!--          });-->
+<!--    };-->
 
-    return {headers, loading, searchText, fetch, load};
+<!--    return {headers, loading, searchText, fetch, load};-->
 
-  },
+<!--  },-->
 
-  computed: {
-    ...mapState(useMeditationStore, ["items", "meta"]),
-  },
+<!--  computed: {-->
+<!--    ...mapState(useMeditationStore, ["items", "meta"]),-->
+<!--  },-->
+<!--};-->
+<!--</script>-->
+
+<script setup>
+import {ref} from 'vue';
+import {useMeditationStore} from '~/stores/meditation.js';
+// import {useSearchQuery, useMetaData} from '~/utils/pagination.js';
+import useSearchQuery from "~/composables/useSearchQuery.js";
+import useMetaData from "~/composables/useMetaData.js";
+
+const loading = ref(true);
+const searchText = ref('');
+const headers = [
+  {key: 'title', title: 'Title', align: 'start', sortable: true},
+  {key: 'set', title: 'Type', sortable: false},
+  {key: 'category', title: 'Category', sortable: false},
+  {key: 'description', title: 'Description', sortable: true},
+  {key: 'lessons_count', title: 'Quantity', sortable: true, align: 'center'},
+  {key: 'thumbnail', title: 'Picture', sortable: false, align: 'center'},
+  {key: 'price', title: 'Price', sortable: true},
+  {key: 'actions', title: '', sortable: false},
+];
+
+const {items,meta} = useMeditationStore();
+const {searchFields} = useSearchQuery();
+const {perPage, currentPage, total} = useMetaData(meta);
+const load = (options) => {
+  loading.value = true;
+  const conditions = [{key: 'title', operator: 'like', value: searchText.value}];
+  useSearchQuery(conditions); // Update search and searchFields
+
+
+  useMeditationStore().fetch(
+      options.page,
+      options.itemsPerPage,
+      options.sortBy?.[0]?.key,
+      options.sortBy?.[0]?.order,
+      searchFields.value
+  ).finally(() => {
+    loading.value = false;
+  });
 };
+
+
+
 </script>
--- utils/pagination.js
+++ utils/pagination.js
@@ -1,18 +1,38 @@
-export function searchQuery(items = []) {
-    let search = "";
-    let searchFields = "";
-    items.forEach((item) => {
-        searchFields += `${item.key}:${item.operator};`;
-        search += `${item.key}:${item.value};`;
-    });
-    return { search, searchFields };
-}
+// export function searchQuery(items = []) {
+//     let search = "";
+//     let searchFields = "";
+//     items.forEach((item) => {
+//         searchFields += `${item.key}:${item.operator};`;
+//         search += `${item.key}:${item.value};`;
+//     });
+//     return { search, searchFields };
+// }
+//
+// export function metaData(meta = null) {
+//
+//     return {
+//         per_page: meta?.per_page || 15,
+//         current_page: meta?.current_page || 1,
+//         total: meta?.total || 0
+//     };
+// }
 
-export function metaData(meta = null) {
-
-    return {
-        per_page: meta?.per_page || 15,
-        current_page: meta?.current_page || 1,
-        total: meta?.total || 0
-    };
-}
+//
+// export function useSearchQuery(items = []) {
+//     let search = '';
+//     let searchFields = '';
+//     items.forEach((item) => {
+//         searchFields += `${item.key}:${item.operator};;`;
+//         search += `${item.key}:${item.value};;`;
+//     });
+//     return {search, searchFields};
+// }
+//
+//
+// export function useMetaData(meta = null) {
+//     const perPage = toRef(meta || { per_page: 15 }, 'per_page');
+//     const currentPage = toRef(meta || { current_page: 1 }, 'current_page');
+//     const total = toRef(meta || { total: 0 }, 'total');
+//
+//     return { perPage, currentPage, total };
+// }
