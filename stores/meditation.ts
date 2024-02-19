ce50fc237e8fbbb5bcd1fc6b1bc4288fdeeac814
JalalLinuX <smjjalalzadeh93@gmail.com>
2024-02-19 10:46:11 +0330

    Update meditation.ts

--- stores/meditation.ts
+++ stores/meditation.ts
@@ -12,6 +12,7 @@ export const useMeditationStore = defineStore('meditation', {
     }),
     actions: {
         async paginate(queryParam: QueryParams) {
+            this.items.clear()
             const {data, meta} = await useApi().paginate<Course>('/admin/v1/course-meditation', queryParam)
             data.forEach(entity => this.items.set(entity.uuid, entity))
             this.meta = meta
