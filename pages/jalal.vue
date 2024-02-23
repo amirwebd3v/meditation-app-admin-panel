518e29262818208e778240110a06e2bc95a57e5d
JalalLinuX <smjjalalzadeh93@gmail.com>
2024-02-23 19:21:14 +0330

    fix table-server.vue

--- pages/jalal.vue
+++ pages/jalal.vue
@@ -1,13 +1,9 @@
 <template>
   <v-container fluid>
     <table-server
-        :total="meta?.total || 0"
         :items="items"
-        :page="table.page"
-        :per-page="table.perPage"
+        :total="meta?.total || 0"
         :fetch="useMeditationStore().paginate"
-        default-sort-key="created_at"
-        default-sort-order="desc"
         :searchable-fields="[
           {field: 'title', operator: 'like'},
           {field: 'description', operator: 'like'},
@@ -22,16 +18,6 @@
 <script setup lang="ts">
 
 const {items, meta} = storeToRefs(useMeditationStore())
-const table = ref({
-  page: 1,
-  perPage: 5,
-  searchableFields: [
-    {field: 'title', operator: 'like'},
-    {field: 'description', operator: 'like'},
-    {field: 'categories.slug', operator: 'in'},
-    {field: 'categories.name', operator: 'like'},
-  ]
-})
 
 </script>
 
