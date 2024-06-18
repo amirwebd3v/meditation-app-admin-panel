076606859cf84ed1d8d8d736ae74ce50b545fec2
AmirHossein <amirhidevs@gmail.com>
2024-06-19 02:28:34 +0330

    add client layout

---
+++ layouts/client.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+useHead({
+  title: 'OmniWell'
+})
+
+
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-app>
+</template>
+
+<style lang="scss">
+@import '@/assets/scss/style.scss';
+</style>
