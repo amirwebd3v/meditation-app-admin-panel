82f077a93f7980a6f17efd3fa573bfc22f7d1ff6
AmirHossein <amirhidevs@gmail.com>
2024-06-19 02:27:31 +0330

    fix all-1 bugs in useInputHasChanges

--- composables/useInputHasChanges.ts
+++ composables/useInputHasChanges.ts
@@ -8,11 +8,12 @@ export default function useInputHasChanges<T extends object>
     resetHasChanges: (initialState ?: T, preview?: Ref<MediaPreview> | null, pictureFile ?: Ref<File[]> | null, trackFile ?: Ref<File[]> | null) => void;
 } {
 
+    // console.log('objectToWatch', objectToWatch)
 
     const hasChanges = ref(false);
     const textFieldsIs = ref(false);
     const thumbnailIs = ref(false);
-    const trackIs = ref({source:false,duration:false});
+    const trackIs = ref({source: false, duration: false});
 
     let oldValObj = JSON.stringify(objectToWatch, (key, value) => {
         if (value === null) return '';
@@ -31,111 +32,109 @@ export default function useInputHasChanges<T extends object>
                 return value;
             });
 
+
             const oldObj = JSON.parse(oldValObj);
             const newObj = JSON.parse(newValObj);
 
-            const { thumbnail: oldThumbnail = null, source: oldSource = null, duration: oldDuration = null, ...filteredOld } = oldObj;
-            const { thumbnail: newThumbnail = null, source: newSource = null, duration: newDuration = null,...filteredNew } = newObj;
+
+            const {
+                thumbnail: oldThumbnail = null,
+                source: oldSource = null,
+                duration: oldDuration = null,
+                ...filteredOld
+            } = oldObj;
 
 
+            const {
+                thumbnail: newThumbnail = null,
+                source: newSource = null,
+                duration: newDuration = null,
+                ...filteredNew
+            } = newObj;
 
-            const emptyValuesInOldTextFields = Object.values(filteredOld).filter((value) => value === null || value === '').length;
-            const oldTextFieldsHasEmpty = emptyValuesInOldTextFields > 1;
+
+            const isFormForEditing =
+                Object.values(filteredOld).filter(
+                    (value) => value === null || value === '').length === 1 ||
+                Object.values(filteredOld).filter(
+                    (value) => value === null || value === '').length === 0;
+
 
             const AllFieldsChanges = () => {
+
+
                 const textFieldsChanged = JSON.stringify(filteredNew) !== JSON.stringify(filteredOld);
-                const thumbnailChanged = newThumbnail !== oldThumbnail;
-                const sourceChanged = newSource !== oldSource;
-                const durationChanged = newDuration !== oldDuration;
-                console.log(sourceChanged && !newSource.endsWith('.mp3'))
+                textFieldsIs.value = textFieldsChanged;
+                const youtubeUrlRegex = /^(https?:\/\/)?(www\.|youtu\.be\/|youtube\.com\/watch\?v=)([a-zA-Z0-9_-]+)(\?.*)?$/;
 
-                if(sourceChanged && !newSource.endsWith('.mp3')){
-                    const youtubeRegex = /^(https?:\/\/)?(www\.|youtu\.be\/|youtube\.com\/watch\?v=)([a-zA-Z0-9_-]+)(\?.*)?$/;
-                    return youtubeRegex.test(newSource)
-                }
+                // console.log('=================================');
+                // console.log('textFieldsChanged:', textFieldsChanged);
+                // console.log('isFormForEditing:', isFormForEditing);
+                // console.log('newThumbnail && oldSource === newSource:', (newThumbnail && oldSource === newSource));
+
+                                // Determine the type of form based on the keys of oldObj
+                const hasSourceField = 'source' in oldObj;
+                const hasThumbnailField = 'thumbnail' in oldObj;
 
 
-                // If text fields changed, and either thumbnail or source changed, and old text field was empty
-                if (textFieldsChanged && (thumbnailChanged || (sourceChanged && durationChanged)) && oldTextFieldsHasEmpty) {
-                    // console.log('Text fields and thumbnail/source changed, and at least one old text field was empty');
-                    textFieldsIs.value = true;
-                    if(thumbnailChanged){
-                        thumbnailIs.value = true;
-                    } else if(sourceChanged){
-                        trackIs.value.source = true;
-                        trackIs.value.duration = true;
-                    } else {
-                        thumbnailIs.value = true;
-                        trackIs.value.source = true;
-                        trackIs.value.duration = true;
+                                // Check if thumbnail or source has changed
+                let thumbnailOrSourceChanged = false;
+
+                if (!isFormForEditing) {
+                                // Case 1: Form for adding data with only source field
+                    if (hasSourceField && !hasThumbnailField) {
+                        const isValidSource = !newSource?.endsWith('.mp3') && (!newSource || youtubeUrlRegex.test(newSource));
+                        thumbnailOrSourceChanged = newSource && isValidSource || newDuration;
+                        trackIs.value.source = !!newSource && isValidSource;
+                        trackIs.value.duration = !!newDuration;
+                        // console.log('Case 1: Form for adding data with only source field');
+                        // console.log('newSource:', newSource);
+                        // console.log('newDuration:', newDuration);
                     }
 
-                    return true;
-                }
-
-                // If text fields changed, but neither thumbnail nor source changed, and old text field was empty
-                if (textFieldsChanged && !(thumbnailChanged || (sourceChanged && durationChanged)) && oldTextFieldsHasEmpty) {
-                    // console.log('Text fields changed, but neither thumbnail nor source changed, and at least one old text field was empty');
-                    textFieldsIs.value = true;
-                    if(thumbnailChanged){
-                        thumbnailIs.value = false;
-                    } else if(sourceChanged){
-                        trackIs.value.source = false;
-                        trackIs.value.duration = false;
-                    } else {
-                        thumbnailIs.value = false;
-                        trackIs.value.source = false;
-                        trackIs.value.duration = false;
+                                // Case 2: Form for adding data with only thumbnail field
+                    else if (!hasSourceField && hasThumbnailField) {
+                        thumbnailOrSourceChanged = newThumbnail;
+                        thumbnailIs.value = !!newThumbnail;
+                        // console.log('Case 2: Form for adding data with only thumbnail field');
+                        // console.log('newThumbnail:', newThumbnail);
                     }
-                    return false;
-                }
 
+                                // Case 3: Form for adding data with both source and thumbnail fields
+                    else if (hasSourceField && hasThumbnailField) {
+                        const isValidSource = !newSource?.endsWith('.mp3') && (!newSource || youtubeUrlRegex.test(newSource));
+                        thumbnailOrSourceChanged = newThumbnail || (newSource && isValidSource && newDuration);
+                        thumbnailIs.value = !!newThumbnail;
+                        trackIs.value.source = !!newSource && isValidSource;
+                        trackIs.value.duration = !!newDuration;
 
-                // If text fields didn't change, but either thumbnail or source changed, and old text field was empty
-                if (!textFieldsChanged && (thumbnailChanged || (sourceChanged && durationChanged)) && oldTextFieldsHasEmpty) {
-                    // console.log('Text fields didn\'t change, but either thumbnail or source changed, and at least one old text field was empty');
-                    textFieldsIs.value = false;
-                    if(thumbnailChanged){
-                        thumbnailIs.value = true;
-                    } else if(sourceChanged){
-                        trackIs.value.source = true;
-                        trackIs.value.duration = true;
-                    } else {
-                        thumbnailIs.value = true;
-                        trackIs.value.source = true;
-                        trackIs.value.duration = true;
+                        // console.log('Case 3: Form for adding data with both source and thumbnail fields');
+                        // console.log('newThumbnail:', !!newThumbnail);
+                        // console.log('newSource:', !!newSource);
+                        // console.log('newDuration:', !!newDuration);
                     }
-                    return false;
+
+                    return textFieldsChanged && thumbnailOrSourceChanged;
+                }
+                else if (isFormForEditing) {
+                    const isValidSource = !newSource?.endsWith('.mp3') && (!newSource || youtubeUrlRegex.test(newSource));
+                    if (hasSourceField) {
+                        return isValidSource && (oldValObj !== newValObj)
+                    }
+                    return (oldValObj !== newValObj);
                 }
 
+                return textFieldsChanged || thumbnailOrSourceChanged;
 
-                // If old text fields were not empty
-                if (!oldTextFieldsHasEmpty) {
-                    // console.log('No old text fields were empty');
-                    return oldValObj !== newValObj;
-                }
-
-
-                if(sourceChanged){
-
-                }
-
-                // console.log('No changes detected');
-                textFieldsIs.value = false;
-                thumbnailIs.value = false;
-                trackIs.value.source = false;
-                trackIs.value.duration = false;
-                return false;
-            };
-
-            hasChanges.value = AllFieldsChanges();
+            }
+                                // Determine the value of hasChanges based on textFields and thumbnail/source changes
+            hasChanges.value = AllFieldsChanges()
+            // console.log('hasChanges.value:', hasChanges.value);
 
         },
 
-        { deep: true }
-
+        {deep: true}
     );
-
 
 
     function resetHasChanges(initialState?: T,
@@ -165,5 +164,6 @@ export default function useInputHasChanges<T extends object>
 
     return {hasChanges, resetHasChanges}
 }
+
 
 
