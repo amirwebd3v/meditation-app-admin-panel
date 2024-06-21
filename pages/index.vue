716fe04d1ece42ac82609f017fead53d2dcb5bfa
AmirHossein <amirhidevs@gmail.com>
2024-06-21 18:52:54 +0330

    disable client layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 definePageMeta({
-  layout: 'client'
+  // layout: 'client'
 })
+
+
 </script>
 
 <template>
-  <v-container fluid>
-    <v-col >
-      <NuxtLink to="/auth/login" class="text-white text-decoration-none">Welcome to the OmniWell...</NuxtLink>
-    </v-col>
-  </v-container>
+<!--  <v-container fluid>-->
+<!--    <v-col >-->
+<!--      <NuxtLink to="/auth/login" class="text-white text-decoration-none">Welcome to the OmniWell...</NuxtLink>-->
+<!--    </v-col>-->
+<!--  </v-container>-->
 </template>
